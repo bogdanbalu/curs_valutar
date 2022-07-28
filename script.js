42e33bc9cd8af824228d3363b8fd772ea616043f
Bogdan Balu <bogdan_va2006@yahoo.com>
2022-07-28 20:24:49 +0300

    Update functionality javascript

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-const EUR = 4.94;   //1 * EURO
+const EUR = 5;   //1 * EURO
 const USD = 4.81;   // 1 * DOLAR
 const CHF = 4.99;    // 1 * Franc elvetian
+const backSpace = "Backspace";
+const deleteButton = 'Delete';
 
-const getValueInput = () => {
-    return document.getElementById("convertor").value;
+const convertorInput = document.getElementById("convertor");
+
+convertorInput.addEventListener("keydown", () => {
+    let currentValue = storageData();
+    console.log(currentValue);
+
+    // calcEur(currentValue);
+    // calcUsd(currentValue);
+    // calcCHF(currentValue);
+});
+
+const storageData = () => {
+    return convertorInput.value;
 }
-
-const inputValue = getValueInput();
-console.log(inputValue);
 
 const calcEur = (value) => {
     return EUR * value;
 }
 
-const getEur = calcEur(inputValue);
-
 const calcUsd = (value) => {
     return USD * value;
 }
-
-const getUsd = calcUsd(inputValue);
 
 const calcCHF = (value) => {
     return CHF * value;
 }
 
-const getChf = calcCHF(inputValue);
-console.log(getChf);
+const clearInput = (key) => {
+    return key === backSpace || key === deleteButton ? null : true
+}
+
+
+
+
